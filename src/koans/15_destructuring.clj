83fa0e7685a619a2d6953328e162d8a0abb95a51
Otávio Pace <otaviopp8@gmail.com>
2019-09-10 23:52:44 -0300

    Add solutions for 15_destructuring

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -8,37 +8,37 @@
 
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
-  (= __ ((fn [[a b]] (str b a))
+  (= ":bar:foo" ((fn [[a b]] (str b a))
          [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "An Oxford comma list of apples, "
           "oranges, "
           "and pears.")
-     ((fn [[a b c]] __)
+     ((fn [[a b c]] (str "An Oxford comma list of " a ", " b ", and " c "."))
       ["apples" "oranges" "pears"]))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-       __))
+       (apply str (interpose " aka " (conj aliases (str first-name " " last-name))))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+       (hash-map :original-parts full-name :named-parts (hash-map :first (first full-name) :last (last full-name)))))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+       (apply str (interpose ", " (list street-address city state)))))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [:street-address :city :state]} test-address]
+       (apply str (interpose ", " (list street-address city state)))))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     ((fn [[t tson] {:keys [:street-address :city :state]}] (apply str (interpose ", " (list (str t " " tson) street-address city state)))) ["Test" "Testerson"] test-address)))
